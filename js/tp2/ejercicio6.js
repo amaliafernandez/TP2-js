f9b125a5c6b8270641e964db92fc8c1ff8f9fd82
amalia fernandez <m.amalia.fernandez@gmail.com>
2026-06-22 10:59:29 -0300

    realice ejercicio 6, piramide de numeros  con un bucle, for

---
+++ js/tp2/ejercicio6.js
@@ -0,0 +1,18 @@
+//Realizar un script que escriba una piramide del 1 al 30 de la siguiennte forma:
+//1
+//22
+//333
+//4444
+//55555
+//666666
+//7777777 ....
+
+
+for (let i = 1; i <= 30; i++) {
+    let fila = ""; // variable que acumula la fila
+
+    for (let repet = 1; repet <= i; repet++) {
+        fila += i; // le pego el número i, tantas veces como diga j
+    }
+    console.log(fila);
+}
